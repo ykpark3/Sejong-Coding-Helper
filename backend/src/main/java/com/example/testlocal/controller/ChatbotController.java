03f2b70ef1c0d73b2d2bfc42a6951bd44977d86b
ykpark <ykpark183@gmail.com>
2021-09-29 01:57:51 +0900

    [ADD] 챗봇에서 값 받아오기

--- backend/src/main/java/com/example/testlocal/controller/ChatbotController.java
+++ backend/src/main/java/com/example/testlocal/controller/ChatbotController.java
@@ -2,8 +2,8 @@ package com.example.testlocal.controller;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -34,7 +34,7 @@ public class ChatbotController {
         String message =  getReqMessage(chatMessage);
         String encodeBase64String = makeSignature(message, secretKey);
 
-        //api서버 접속 (서버 -> 서버 통신)
+        // api서버 접속 (서버 -> 서버 통신)
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "application/json;UTF-8");
@@ -58,26 +58,36 @@ public class ChatbotController {
             String decodedString;
             String jsonString = "";
             while ((decodedString = in.readLine()) != null) {
-                jsonString = decodedString;
+                jsonString = decodedString; //jsonString에 값 받아오기
             }
 
             //받아온 값을 세팅하는 부분
-            JSONParser jsonparser = new JSONParser();
+            //JSONParser jsonParser = new JSONParser();
+            JSONParser jsonParser = new JSONParser(jsonString);
             try {
-                JSONObject json = (JSONObject)jsonparser.parse(jsonString);
-                JSONArray bubblesArray = (JSONArray)json.get("bubbles");
+                JSONObject jsonObject = (JSONObject)jsonParser.parse();
+
+
+                // "bubbles": [ {"type": "text",
+                //"data" : { "description" : "postback text of welcome action" } } ],
+                //"event": "open"
+                JSONArray bubblesArray = (JSONArray)jsonObject.get("bubbles");
                 JSONObject bubbles = (JSONObject)bubblesArray.get(0);
+
                 JSONObject data = (JSONObject)bubbles.get("data");
                 String description = "";
                 description = (String)data.get("description");
                 chatMessage = description;
+
             } catch (Exception e) {
                 System.out.println("error");
                 e.printStackTrace();
             }
 
             in.close();
-        } else {  // 에러 발생
+        }
+
+        else {  // 에러 발생
             chatMessage = con.getResponseMessage();
         }
         return chatMessage;
@@ -86,51 +96,54 @@ public class ChatbotController {
     //보낼 메세지를 네이버에서 제공해준 암호화로 변경해주는 메소드
     public static String makeSignature(String message, String secretKey) {
 
-        String encodeBase64String = "";
+        String signatureHeader = "";
 
+        // 요청 Signature 헤더
         try {
             byte[] secrete_key_bytes = secretKey.getBytes("UTF-8");
 
-            SecretKeySpec signingKey = new SecretKeySpec(secrete_key_bytes, "HmacSHA256");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secrete_key_bytes, "HmacSHA256");
             Mac mac = Mac.getInstance("HmacSHA256");
-            mac.init(signingKey);
+            mac.init(secretKeySpec);
 
-            byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
-            encodeBase64String = Base64.encodeBase64String(rawHmac);
+            byte[] signature  = mac.doFinal(message.getBytes("UTF-8"));
+            signatureHeader = Base64.encodeBase64String(signature );
 
-            return encodeBase64String;
+            return signatureHeader;
 
         } catch (Exception e){
             System.out.println(e);
         }
 
-        return encodeBase64String;
+        return signatureHeader;
 
     }
 
     //보낼 메세지를 네이버 챗봇에 포맷으로 변경해주는 메소드
-    public static String getReqMessage(String voiceMessage) {
+    public static String getReqMessage(String sendMessage) {
 
         String requestBody = "";
 
         try {
 
-            JSONObject obj = new JSONObject();
+            JSONObject jsonObject = new JSONObject();
 
             long timestamp = new Date().getTime();
 
-            System.out.println("##"+timestamp);
+            //System.out.println("##"+timestamp);
 
-            obj.put("version", "v2");
-            obj.put("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");
-            obj.put("timestamp", timestamp);
+            jsonObject.put("version", "v2");
+            // userid는 유일한 키값이면 아무거나 가능
+            // 일단 예시 값 넣어두기
+            jsonObject.put("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");  
+            jsonObject.put("timestamp", timestamp);
 
             JSONObject bubbles_obj = new JSONObject();
 
             bubbles_obj.put("type", "text");
 
             JSONObject data_obj = new JSONObject();
-            data_obj.put("description", voiceMessage);
+            data_obj.put("description", sendMessage);
 
             bubbles_obj.put("type", "text");
             bubbles_obj.put("data", data_obj);
@@ -138,10 +151,10 @@ public class ChatbotController {
             JSONArray bubbles_array = new JSONArray();
             bubbles_array.add(bubbles_obj);
 
-            obj.put("bubbles", bubbles_array);
-            obj.put("event", "send");
+            jsonObject.put("bubbles", bubbles_array);
+            jsonObject.put("event", "send");
 
-            requestBody = obj.toString();
+            requestBody = jsonObject.toString();
 
         } catch (Exception e){
             System.out.println("## Exception : " + e);
