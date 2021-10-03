62cb6e687b023f3c486c1e2836faad413e0417ba
taesunpark <tovbskvb@daum.net>
2021-10-04 04:33:47 +0900

    [add] 조교 DTO 추가

--- backend/src/main/java/com/example/testlocal/domain/dto/AssistantDTO.java
+++ backend/src/main/java/com/example/testlocal/domain/dto/AssistantDTO.java
@@ -1,9 +1,13 @@
 package com.example.testlocal.domain.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AssistantDTO {
+    private Long userId;
+    private String studentEmail;
 }
