8b60946f2ba87c3df4ab63176541d5002df463d5
taesunpark <tovbskvb@daum.net>
2021-10-13 18:08:36 +0900

    [merge]

--- backend/src/main/java/com/example/testlocal/config/SecurityConfig.java
+++ backend/src/main/java/com/example/testlocal/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
 
         //configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://3.141.167.159");
+        configuration.addAllowedOrigin("https://sju-coding.helper");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
