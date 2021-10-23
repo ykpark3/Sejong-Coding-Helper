e291e8f0877d0bb0f8a25f54934dff493621f125
jsb100800@naver.com <jsb100800@naver.com>
2021-10-24 03:28:50 +0900

    [ADD] 코드 컴파일러 페이지

--- backend/src/main/java/com/example/testlocal/controller/ChatbotRoomController.java
+++ backend/src/main/java/com/example/testlocal/controller/ChatbotRoomController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -63,6 +64,11 @@ public class ChatbotRoomController {
 
         HttpSession session = request.getSession();
         session.setAttribute("bRoomId",roomId);
+    }
+
+    @PostMapping("/testtt")
+    public void test(@RequestBody Map<String, String> map){
+        System.out.println(map.get("code"));
     }
 
 }
--- frontend/src/contents/Curriculum.js
+++ frontend/src/contents/Curriculum.js
@@ -1,26 +1,87 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import VerticalHeader from './VerticalHeader';
 import HorizontalHeader from './HorizontalHeader';
 import Editor from '@monaco-editor/react';
 import '../css/CodingEditor.css';
-
+import axios from 'axios';
+import { API_BASE_URL } from './utils/Constant';
 const Curriculum = () => {
+  const editorRef = useRef(null);
+
+  function showValue() {
+    //alert(editorRef.current.getValue());
+    const code = editorRef.current.getValue();
+    axios
+      .post(
+        API_BASE_URL + '/compiler/c',
+        { code: code },
+        {
+          headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json',
+          },
+          withCredentials: true,
+        },
+      )
+      .then((res) => {})
+      .catch((res) => {
+        console.log(res);
+        alert('일시적 오류가 발생했습니다. 다시 시도해주세요.');
+      });
+  }
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
   return (
     <div id="editorContainer">
       <VerticalHeader />
       <HorizontalHeader />
 
       <div id="editorBox">
-        <h1>코딩하기</h1>
+        <div id="codingTopNav">
+          <h1>코딩하기</h1>
+          <button
+            onClick={() => {
+              showValue();
+            }}
+          >
+            컴파일
+          </button>
+        </div>
 
-        <Editor className="codingEditor"
-          theme="vs-dark"
-        height="90vh"
-          defaultLanguage="c++"
-          defaultValue="// some comment"
-        />
+        <div id="codingInnerBox">
+          <Editor
+            className="codingEditor"
+            theme="vs-dark"
+            defaultLanguage="c"
+            onMount={handleEditorDidMount}
+            defaultValue="//coding here"
+          />
+        </div>
       </div>
-      <div id="outputBox"><h1>qwe</h1></div>
+      <div id="outputBox">
+        <h1>입력값 설정</h1>
+        <div id="inputLine"></div>
+        <h1>결 과</h1>
+        <div id="outputLine">
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>            <h3>qweqwe</h3>
+            <h3>qweqwe</h3>
+
+        </div>
+      </div>
     </div>
   );
 };
